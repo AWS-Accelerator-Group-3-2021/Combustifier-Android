d5a148db1a961bbc5b1bc77124ba76d5a1a12a7f
yisian <yisiankevin@gmail.com>
2021-12-15 15:29:48 +0800

    got the button aligned woo
    lesgo

--- app/src/main/java/com/awsgroup3/combustifier/ContentScreens.kt
+++ app/src/main/java/com/awsgroup3/combustifier/ContentScreens.kt
@@ -1,15 +1,14 @@
 package com.awsgroup3.combustifier
 
 import android.graphics.Paint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.awsgroup3.combustifier.ui.theme.CombustifierTheme
 
 @ExperimentalMaterial3Api
@@ -18,25 +17,23 @@ fun HomeScreen() {
     CombustifierTheme {
         Scaffold(
             topBar = { TopAppBar(pageName = "Home") },
-            content = {}
-        )
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .wrapContentSize(Alignment.Center)
-        ) {
-            Text(
-                text = "Home View Test"
-            )
-            ElevatedButton(
-                onClick = {println("Button was pressed")}
-            ) {
-                Icon(Icons.Filled.Add, contentDescription = null)
-                Text("New Check")
+            content = {},
+            floatingActionButtonPosition = FabPosition.End,
+            floatingActionButton = {
+                ElevatedButton(
+                    modifier = Modifier
+                        .padding(32.dp,32.dp,0.dp,80.dp)
+                        .wrapContentWidth(),
+                    onClick = { println("Button was pressed") }
+                ) {
+                    Icon(Icons.Filled.Add, contentDescription = null)
+                    Text("New Check")
+                }
             }
-        }
+        )
     }
 }
+
 
 @ExperimentalMaterial3Api
 @Composable
@@ -56,4 +53,9 @@ fun MeasurementScreen() {
             )
         }
     }
+}
+
+
+@Composable
+fun CameraScreen() {
 }
--- app/src/main/java/com/awsgroup3/combustifier/NavigationItems.kt
+++ app/src/main/java/com/awsgroup3/combustifier/NavigationItems.kt
@@ -1,7 +1,10 @@
 package com.awsgroup3.combustifier
 
+import androidx.navigation.Navigation
+
 
 sealed class NavigationItem(var route: String, var title: String) {
     object Home : NavigationItem("home", "Home")
     object Measurement : NavigationItem("measurement", "Measurement")
+    object Camera : NavigationItem("camera", "Camera")
 }
