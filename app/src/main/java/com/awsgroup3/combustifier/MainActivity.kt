4742c8a89ee60ae3bbd26dac79a369662d46f1cc
yisian <yisiankevin@gmail.com>
2021-12-20 16:29:40 +0800

    splash screen wow sheesh

--- app/src/main/java/com/awsgroup3/combustifier/MainActivity.kt
+++ app/src/main/java/com/awsgroup3/combustifier/MainActivity.kt
@@ -1,46 +1,47 @@
 package com.awsgroup3.combustifier
 
-import android.content.Context
-import android.content.ContextWrapper
 import android.graphics.Bitmap
 import android.icu.text.DateFormat.getDateTimeInstance
 import android.os.Bundle
 import android.os.Environment
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
-import androidx.camera.core.impl.utils.ContextUtil.getApplicationContext
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import coil.annotation.ExperimentalCoilApi
 import com.awsgroup3.combustifier.ui.theme.CombustifierTheme
 import com.awsgroup3.combustifier.ui.theme.Typography
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import kotlinx.coroutines.delay
 import java.io.File
 import java.io.FileOutputStream
-import java.io.IOException
-
-import java.text.SimpleDateFormat
-import java.util.*
-
 
 
 class MainActivity : ComponentActivity() {
@@ -51,13 +52,64 @@ class MainActivity : ComponentActivity() {
             CombustifierTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    MainScreen()
+                    ScreenInitializer()
                 }
             }
         }
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScreenInitializer() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "splash_screen"
+    ) {
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
+        // Main Screen
+        composable("main_screen") {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
+    // AnimationEffect
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.7f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                })
+        )
+        delay(3000L)
+        navController.navigate("main_screen")
+    }
+
+    // Image
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.combustifierlogo),
+            contentDescription = "Combustifier",
+            modifier = Modifier.scale(scale.value)
+        )
+    }
+}
 
 @ExperimentalMaterial3Api
 @Composable
