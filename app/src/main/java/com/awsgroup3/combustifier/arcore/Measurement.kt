cf8f6d282549304432b59068d0379297b0f1ef70
yisian <yisiankevin@gmail.com>
2021-12-23 21:23:33 +0800

    ARCORE WORKS YOOOOO
    WTF

--- app/src/main/java/com/awsgroup3/combustifier/arcore/Measurement.kt
+++ app/src/main/java/com/awsgroup3/combustifier/arcore/Measurement.kt
@@ -14,6 +14,7 @@ import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
 import android.widget.*
+import androidx.activity.ComponentActivity
 import com.google.ar.core.*
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.FrameTime
