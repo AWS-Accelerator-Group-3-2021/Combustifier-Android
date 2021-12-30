e7202cbfb25f612ed5ebd07f069631ef712d4d26
yisian <yisiankevin@gmail.com>
2021-12-30 13:01:48 +0800

    committing these before i revert

--- app/src/main/java/com/awsgroup3/combustifier/arcore/Reports.kt
+++ app/src/main/java/com/awsgroup3/combustifier/arcore/Reports.kt
@@ -16,71 +16,11 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import java.util.*
 
-@Composable
-fun AfterMeasuringScreen() {
-    var name by remember { mutableStateOf("")}
-    var address by remember { mutableStateOf("")}
-    val id = UUID.randomUUID().toString()
-    val openDialog = remember { mutableStateOf(true) }
-    val measurementResultForReports = 69.0f
-    if (openDialog.value) {
-        AlertDialog(
-            icon = {Icon(Icons.Filled.Warning, contentDescription = null)},
-            onDismissRequest = {
-                // Dismiss the dialog when the user clicks outside the dialog or on the back
-                // button. If you want to disable that functionality, simply use an empty
-                // onDismissRequest.
-                openDialog.value = false
-            },
-            title = {
-                Text(text = "Submit a Report")
-            },
-            text = {
-                Column() {
-                    OutlinedTextField(
-                        value = name,
-                        onValueChange = { name = it },
-                        label = { Text("your name") },
-                    )
-                    OutlinedTextField(
-                        value = address,
-                        onValueChange = { address = it },
-                        label = { Text("address of offender") },
-                    )
-                    OutlinedTextField(
-                        value = measurementResultForReports.toString(),
-                        onValueChange = {},
-                        readOnly = true,
-                        label = { Text("measurement result") },
-                    )
-                }
-            },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        openDialog.value = false
-                    }
-                ) {
-                    Text("Submit!")
-                }
-            },
-            dismissButton = {
-                TextButton(
-                    onClick = {
-                        openDialog.value = false
-                    }
-                ) {
-                    Text("Cancel")
-                }
-            }
-        )
-    }
-}
+
 
 
 
 @Preview
 @Composable
 fun Preview() {
-    AfterMeasuringScreen()
 }
