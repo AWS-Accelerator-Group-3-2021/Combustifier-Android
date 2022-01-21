85e0b7888fdfaee41bf49e902bbaf19d7d77070e
yisian <yisiankevin@gmail.com>
2022-01-21 15:51:14 +0800

    update homescreen to display the processed images

--- app/src/main/java/com/awsgroup3/combustifier/ContentScreens.kt
+++ app/src/main/java/com/awsgroup3/combustifier/ContentScreens.kt
@@ -1,21 +1,33 @@
 package com.awsgroup3.combustifier
 
+import android.R
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Environment
+import android.util.Log
+import android.widget.ImageView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.awsgroup3.combustifier.ui.theme.CombustifierTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import java.io.File
+
 
 @OptIn(ExperimentalPermissionsApi::class)
 @ExperimentalMaterial3Api
@@ -29,9 +41,7 @@ fun HomeScreen(navController: NavController) {
             },
             floatingActionButtonPosition = FabPosition.End,
             content = {
-                Column() {
-                    ImageCard(null)
-                }
+                ImageCardColumn()
             }
         )
     }
@@ -52,7 +62,9 @@ fun MeasurementScreen(navController: NavController) {
 
 @ExperimentalMaterial3Api
 @Composable
-fun ImageCard(picture: Any?) {
+fun ImageCard(picture: File) {
+    val painter = rememberImagePainter(picture)
+    //convert the file to a painter
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -60,23 +72,43 @@ fun ImageCard(picture: Any?) {
         elevation = 10.dp,
         backgroundColor = Color(0xFF2D2935)
     ) {
-        Column(
-            modifier = Modifier.padding(15.dp)
-        )
-        {
             Row(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.3f)
+                    .padding(16.dp)
             ) {
-                Text("Hello")
-                val image: Painter = painterResource(id = R.drawable.combustifierlogo)
-                Image(painter = image, contentDescription = null)
+                Image(painter, null, alignment = Alignment.CenterStart)
+                Text("combustible",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    textAlign = TextAlign.End
+                )
             }
-        }
+
     }
 }
+
+// for number of images create a imagecard
+// for each image create a row with a text and an image
 
 @ExperimentalMaterial3Api
 @Composable
 fun ImageCardColumn() {
-
+    val folder = File(Environment.getExternalStorageDirectory().toString() + "/Pictures/")
+    Log.d("folderPathName", folder.toString())
+    //return uri of all images in folder
+    val pictures = folder.listFiles()
+    Log.d("pictures", pictures.toString())
+        Column(
+            modifier = Modifier.padding(15.dp)
+        )
+        {
+            for (pic in pictures) {
+                Log.d("file", pic.toString())
+                ImageCard(pic)
+            }
+        }
 }
+
